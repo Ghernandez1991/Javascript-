b24ef69bb22b77a6a5bcdda516230adf6181f492
Ghernandez1991 <49377511+Ghernandez1991@users.noreply.github.com>
2019-08-01 21:35:09 -0500

    Add files via upload
    reuploading with correct file structure

---
+++ 14-Intro-To-JavaScript/Instructions/StarterCode/static/js/index.js
@@ -0,0 +1,17 @@
+var tbody = d3.select("tbody");
+
+//consolse log the ufo data from data.js
+console.log(data);
+
+//loop through data and console log the results
+data.forEach(function(uforeport){
+    console.log(uforeport);
+
+});
+
+data.forEach(function(uforeport){
+    console.log(uforeport);
+    var row = tbody.append("tr");
+});
+
+
